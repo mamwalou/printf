f20fdd685cb4ca50aa52f680e917d05e455b7294
Samuel BELINE <sbeline@e1r11p18.42.fr>
2016-05-13 18:04:49 +0200

    some,norm2

--- srcs/lenght_check2.c
+++ srcs/lenght_check2.c
@@ -6,13 +6,13 @@
 /*   By: sbeline <sbeline@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/13 16:20:36 by sbeline           #+#    #+#             */
-/*   Updated: 2016/05/13 18:02:25 by sbeline          ###   ########.fr       */
+/*   Updated: 2016/05/13 18:04:39 by sbeline          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/my_printf.h"
 
-int				lenght_h(int specifier, t_args *args, t_tabvar *var)
+int					lenght_h(int specifier, t_args *args, t_tabvar *var)
 {
 	if (specifier == 4 || specifier == 6)
 	{
@@ -44,16 +44,18 @@ static int			lenght_z2(int specifier, t_args *args, t_tabvar *var)
 {
 	if (specifier == 9 || specifier == 7 || specifier == 11 || specifier == 12)
 	{
-		var->var_unlonglong= va_arg(args->ap, unsigned long long);
+		var->var_unlonglong = va_arg(args->ap, unsigned long long);
 		if (specifier == 7)
-			return(convert_uloctal(var->var_unlonglong,
+			return (convert_uloctal(var->var_unlonglong,
 					ft_lenghtunlong(var->var_unlonglong)));
 		else if (specifier == 11)
 			return (convert_unlhx(var->var_unlonglong,
 					ft_lenghtunlong(var->var_unlonglong), 0));
 		else if (specifier == 12)
+		{
 			return (convert_unlhx(var->var_unlonglong,
 					ft_lenghtunlong(var->var_unlonglong), 1));
+		}
 		ft_printunlong(var->var_unlonglong);
 		return (ft_lenghtunlong(var->var_unlonglong));
 	}
@@ -66,19 +68,19 @@ static int			lenght_z2(int specifier, t_args *args, t_tabvar *var)
 	return (-1);
 }
 
-int				lenght_z(int specifier, t_args *args, t_tabvar *var)
+int					lenght_z(int specifier, t_args *args, t_tabvar *var)
 {
 	if (specifier == 5 || specifier == 4 || specifier == 6)
 	{
-		var->var_longlong= va_arg(args->ap, long long);
+		var->var_longlong = va_arg(args->ap, long long);
 		ft_printlong(var->var_longlong);
 		return (ft_lenghtlong(var->var_longlong));
 	}
 	else if (specifier == 8)
 	{
-		var->var_longlong= va_arg(args->ap, long long);
-		return(convert_loctal(var->var_longlong,
-			 ft_lenghtlong(var->var_longlong)));
+		var->var_longlong = va_arg(args->ap, long long);
+		return (convert_loctal(var->var_longlong,
+				ft_lenghtlong(var->var_longlong)));
 	}
 	return (lenght_z2(specifier, args, var));
 }
